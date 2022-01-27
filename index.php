fda15d3398479796cfa9fa33801ad5dd91c09118
AntonioJavierGarciaQuesada <fenix23re@hotmail.com>
2022-01-27 20:20:34 +0100

    modify index.php included new case of use

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-use Adsmurai\CoffeeMachine\Console\MakeDrinkCommand;
+use Adsmurai\CoffeeMachine\Console\Application\MakeDrinkCommand;
+use Adsmurai\CoffeeMachine\Console\Application\ShowCashTotal;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 
 $application->add(new MakeDrinkCommand());
+$application->add(new ShowCashTotal);
 
 $application->run();
