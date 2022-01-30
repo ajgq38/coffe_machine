f7d6cb96df9cb4e15c4b5ec545d632cc7bce7db0
AntonioJavierGarciaQuesada <fenix23re@hotmail.com>
2022-01-30 16:16:09 +0100

    Modify function static to constructor

--- src/Console/Infrastructure/MysqlPdoClient.php
+++ src/Console/Infrastructure/MysqlPdoClient.php
@@ -1,32 +1,37 @@
 <?php
 
-namespace Adsmurai\CoffeeMachine\Console\Infrastructure\Bd;
+namespace Adsmurai\CoffeeMachine\Console\Infrastructure;
 
 class MysqlPdoClient
 {
-    private static $pdo;
+    private \PDO $pdo;
     private const HOST = 'localhost';
     private const USER_NAME = 'php';
     private const USER_PASSWORD = 'php';
     private const DB_NAME = 'test';
 
-    public static function getPdo(): \PDO
+    public function __construct()
     {
 
-        if (!(self::$pdo instanceof \PDO)) {
-            $options = [
-                \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                \PDO::ATTR_EMULATE_PREPARES   => false,
-            ];
-            $dsn = "mysql:host=". self::HOST.";dbname=".self::DB_NAME.";charset=utf8";
-            try {
-                self::$pdo = new \PDO($dsn, self::USER_NAME, self::USER_PASSWORD, $options);
-            } catch (\PDOException $e) {
-                throw new \PDOException($e->getMessage(), (int)$e->getCode());
-            }
+        $options = [
+            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $dsn = "mysql:host=". self::HOST.";dbname=".self::DB_NAME.";charset=utf8";
+        try {
+            $this->pdo = new \PDO($dsn, self::USER_NAME, self::USER_PASSWORD, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
         }
-
-        return self::$pdo;
     }
+
+    /**
+     * @return \PDO
+     */
+    public function getPdo(): \PDO
+    {
+        return $this->pdo;
+    }
+
 }
