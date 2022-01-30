d5c2ac1692ae7982d467e47c2a93ff137bb5d6da
AntonioJavierGarciaQuesada <fenix23re@hotmail.com>
2022-01-30 16:19:11 +0100

    Inyection OrderRepository and use MakeDrinkCommand+ showCashTotal

--- index.php
+++ index.php
@@ -6,11 +6,13 @@ require __DIR__.'/vendor/autoload.php';
 
 use Adsmurai\CoffeeMachine\Console\Application\MakeDrinkCommand;
 use Adsmurai\CoffeeMachine\Console\Application\ShowCashTotal;
+use Adsmurai\CoffeeMachine\Console\Infrastructure\OrderRepositoryMysql;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 
-$application->add(new MakeDrinkCommand());
-$application->add(new ShowCashTotal);
+$order_repository = new OrderRepositoryMysql();
+$application->add(new MakeDrinkCommand($order_repository));
+$application->add(new ShowCashTotal($order_repository));
 
 $application->run();
