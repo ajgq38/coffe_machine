8a2c752489ab062e1cbdcac12fccaf765104a9bc
AntonioJavierGarciaQuesada <fenix23re@hotmail.com>
2022-01-30 16:12:38 +0100

    Class MysqlPdoClient modify

--- src/Console/Infrastructure/MysqlPdoClient.php
+++ src/Console/Infrastructure/MysqlPdoClient.php
@@ -1,24 +1,27 @@
 <?php
 
 namespace Adsmurai\CoffeeMachine\Console\Infrastructure\Bd;
-use Adsmurai\CoffeeMachine\Console\Infrastructure\BD\Config\Develop as BD_CONFIG_DEVELOP;
 
 class MysqlPdoClient
 {
     private static $pdo;
+    private const HOST = 'localhost';
+    private const USER_NAME = 'php';
+    private const USER_PASSWORD = 'php';
+    private const DB_NAME = 'test';
 
     public static function getPdo(): \PDO
     {
-        $config_bd = BD_CONFIG_DEVELOP::getParameter();
+
         if (!(self::$pdo instanceof \PDO)) {
             $options = [
                 \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
                 \PDO::ATTR_EMULATE_PREPARES   => false,
             ];
-            $dsn = "mysql:host=".$config_bd["HOST"].";dbname=".$config_bd["DB_NAME"] .";charset=utf8";
+            $dsn = "mysql:host=". self::HOST.";dbname=".self::DB_NAME.";charset=utf8";
             try {
-                self::$pdo = new \PDO($dsn, $config_bd["USER_NAME"], $config_bd["USER_PASSWORD"], $options);
+                self::$pdo = new \PDO($dsn, self::USER_NAME, self::USER_PASSWORD, $options);
             } catch (\PDOException $e) {
                 throw new \PDOException($e->getMessage(), (int)$e->getCode());
             }
