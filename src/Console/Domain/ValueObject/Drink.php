26e776f2925e4e76a74eeb7a6f823cfea3aeac94
AntonioJavierGarciaQuesada <fenix23re@hotmail.com>
2022-01-30 16:15:09 +0100

    Add Constant Error_Invalid_drink

--- src/Console/Domain/ValueObject/Drink.php
+++ src/Console/Domain/ValueObject/Drink.php
@@ -8,8 +8,11 @@ class Drink
     public const PRICE_TEA = 0.4;
     public const PRICE_COFFE = 0.5;
     public const PRICE_CHOCOLATE = 0.6;
+    private const ERROR_IVALID_TYPE = 'The drink type should be tea, coffee or chocolate.';
+
     private string $type;
     private float $price;
+
 
     public function __construct(string $type)
     {
@@ -25,7 +28,7 @@ class Drink
                 $this->price = self::PRICE_CHOCOLATE;
                 break;
             default:
-                throw new Exception('The drink type should be tea, coffee or chocolate.');
+                throw new Exception(self::ERROR_IVALID_TYPE);
         }
         $this->type = $type;
 
